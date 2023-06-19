6f29f9dc2b0ac0df99d671ae0983a978575faa87
Carl Boudard <tcarl771@gmail.com>
2023-06-19 15:04:45 +0200

    use and equip Character's method set

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -1,10 +1,10 @@
+include "includes/inlcudes.hpp"
 
-
-
-
-
-
-Character::Character() { std::cout << "Character constructor called" << std::endl; }
+Character::Character() {
+	std::cout << "Character constructor called" << std::endl;
+	for (short i = 0; i < 4; i++)
+		this->items[i] = nullptr;	
+}
 
 Character::~Character() { std::cout << "Character destructor called" << std::endl; }
 
@@ -14,12 +14,23 @@ Character& Character::operator=(Character& const rhs) { return *this; }
 
 void Character::equip(AMateria* m) {
 	
+	for (short i = 0; i < 4; i++)
+	{
+		if (this->items[i] == nullptr)
+		{
+			items[i] = m;
+			return;
+		}
+	}
 }
 
 void Character::unequip(int idx) {
-    
+	
 }
 
 void Character::use(int idx, Character& target) {
-
+	if (idx < 0 || idx > 3)
+		return;
+	items[idx]->use(*this);
+	
 }
