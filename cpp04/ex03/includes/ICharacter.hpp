527cb9c47a43f45e7a2eef2191adac827290cef3
Carl Boudard <tcarl771@gmail.com>
2023-06-20 22:10:43 +0200

    IMaterialSource implemented + ICharacter.hpp set to starndard norme + ICharacter.cpp removed

--- cpp04/ex03/includes/ICharacter.hpp
+++ cpp04/ex03/includes/ICharacter.hpp
@@ -3,12 +3,7 @@
 
 class ICharacter {
 	public:
-			ICharacter();
 			virtual ~ICharacter();
-			ICharacter(std::string name);
-			ICharacter(ICharacter& const cpy);
-			ICharacter& operator=(ICharacter& const rhs);
-
 			virtual std::string const & getName() const = 0;
 			virtual void equip(AMateria* m) = 0;
 			virtual void unequip(int idx) = 0;
--- cpp04/ex03/includes/IMateriaSource.hpp
+++ cpp04/ex03/includes/IMateriaSource.hpp
@@ -1,2 +1,9 @@
 #pragma once
 #include "includes.hpp"
+
+class IMAteriaSource {
+	public:
+			virtual ~IMateriaSource() = default;
+			learnMateria(AMateria*) = 0;
+			createMateria((std::string const &) = 0;
+}
