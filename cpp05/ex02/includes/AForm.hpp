6afc7738f0dbc77cb182e8e4d47e8fcf4f6c8a97
Carl Boudard <tcarl771@gmail.com>
2023-06-29 04:32:23 +0200

    AForm sublcass init ongoing

---
+++ cpp05/ex02/includes/AForm.hpp
@@ -0,0 +1,42 @@
+
+#pragma once
+#include <iostream>
+#include <cstdint>
+#include <exception>
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class AForm {
+	public:
+			class GradeTooHighException : std::exception {
+					public:
+							virtual const char* what() const throw();
+			};
+
+			class GradeTooLowException : std::exception {
+					public:
+							virtual const char* what() const throw();
+			};
+
+			AForm();
+			AForm(const std::string& name, const int16_t grade);
+			~AForm();
+			AForm(const AForm& cpy);
+			AForm& operator=(const AForm& rhs);
+
+			void			beSigned(Bureaucrat& bureaucrat);
+			virtual void	execute(const Bureaucrat& executor) const = 0;
+
+			const std::string	getName() const;
+			int16_t				getGrade() const;
+			int16_t				getExecGrade() const;
+			bool				getIsSigned() const;
+	private:
+			const std::string	_name;
+			const int16_t		_grade;
+			const int16_t		_execGrade;
+			bool				_isSigned;
+};
+
+std::ostream& operator<<(std::ostream& os, const AForm& rhs);
---
+++ cpp05/ex02/includes/Bureaucrat.hpp
@@ -0,0 +1,40 @@
+
+#pragma once
+#include <iostream>
+#include <cstdint>
+#include <exception>
+#include "AForm.hpp"
+
+class AForm;
+
+class Bureaucrat {
+	public:
+			class GradeTooHighException : public std::exception {
+					public:
+							virtual const char* what() const throw();
+			};
+
+			class GradeTooLowException : public std::exception {
+					public:
+							virtual const char* what() const throw();
+			};
+
+			Bureaucrat();
+			Bureaucrat(const std::string name, int16_t grade);
+			~Bureaucrat();
+			Bureaucrat (const Bureaucrat& cpy);
+			Bureaucrat& operator=(const Bureaucrat& rhs);
+
+			void 				upgradeGrade();
+			void				demoteGrade();
+			void				signForm(const AForm& form) const;
+
+			std::string const	getName() const;
+			int16_t				getGrade() const;
+	private:
+			const std::string	_name;
+			int16_t				_grade;
+
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& rhs);
