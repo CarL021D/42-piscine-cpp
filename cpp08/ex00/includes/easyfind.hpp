cf8e416230ead3d11bd575d5f413bfb2340d6aec
Carl BOUDARD <caboudar@paul-f5Br2s1.clusters.42paris.fr>
2023-07-25 17:35:23 +0200

    container iterator set

--- cpp08/ex00/includes/easyfind.hpp
+++ cpp08/ex00/includes/easyfind.hpp
@@ -2,10 +2,22 @@
 #include <iostream>
 #include <stdexcept>
 
+class NoOccurenceException : public std::exception {
+	public:
+			const char* what() const throw() {
+				return "No Occurrence exception thrown";
+			}
+};
+
 template <typename T>
 void easyFind(const T& container, int nb) {
+	typename T::const_iterator it;
 
-	for (typename Container::const_iterator it = container.begin(); it != container.end(); it++)
-		
-
+	for (it = container.begin(); it != container.end(); ++it) {
+		if (*it == nb) {
+			std::cout << "Occurrence of number: " << nb << " found at index " << std::distance(container.begin(), it) << std::endl;
+			return;
+		}
+	}
+	throw NoOcccurenceException();
 }
