73e13f0536901b24915e70e0e2f81ec19e9ab15b
Carl BOUDARD <caboudar@paul-f5Ar5s1.clusters.42paris.fr>
2023-07-31 20:39:42 +0200

    tests set

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -3,41 +3,42 @@
 
 int main()
 {
-	// Animal* animalArr[4];
+	Animal* animalArr[4];
 
-	// std::cout << std::endl << "Animal array construction:" << std::endl;
-	// for (short i = 0; i < 4 ; i++) {
-	// 	if (i < 2)
-	// 		animalArr[i] = new Dog();
-	// 	else
-	// 		animalArr[i] = new Cat();
-	// }
+	std::cout << std::endl << "Animal array construction:" << std::endl;
+	for (short i = 0; i < 4 ; i++) {
+		if (i < 2)
+			animalArr[i] = new Dog();
+		else
+			animalArr[i] = new Cat();
+	}
 
-	// std::cout << std::endl << "Display Idea:" << std::endl;
-	// std::cout << animalArr[0]->getType() << std::endl;
-	// animalArr[0]->displayIdea(4);
-	// std::cout << std::endl;
-	// std::cout << animalArr[3]->getType() << std::endl;
-	// animalArr[3]->displayIdea(100);
-	// std::cout << std::endl;
-
-	// std::cout << "Animal array destruction:" << std::endl;
-	// for (short i = 0; i < 4 ; i++)
-	// 	delete animalArr[i];
-	// std::cout << std::endl << std::endl;
-
-
-	// Shallow copy 
-	Dog *dog1 = new Dog();
-	dog1->setNewIdea(21, "Nerf Jp");
-	Dog dog2(*dog1);
-	std::cout << "---" << std::endl;
-	dog1->displayIdea(21);
-	dog2.displayIdea(21);
+	std::cout << std::endl << "Display Idea:" << std::endl;
+	std::cout << animalArr[0]->getType() << std::endl;
+	animalArr[0]->displayIdea(4);
 	std::cout << std::endl;
-	delete dog1;
+	std::cout << animalArr[3]->getType() << std::endl;
+	animalArr[3]->displayIdea(100);
 	std::cout << std::endl;
-	dog2.displayIdea(21);
+
+	std::cout << "Animal array destruction:" << std::endl;
+	for (short i = 0; i < 4 ; i++)
+		delete animalArr[i];
+	std::cout << std::endl;
+
+	// // TEST 2 Shallow copy 
+	// Dog *dog1 = new Dog();
+	// dog1->setNewIdea(21, "Nerf Jp");
+	// Dog dog2(*dog1);
+	// std::cout << "---" << std::endl;
+	// dog1->displayIdea(21);
+	// dog2.displayIdea(21);
+	// std::cout << std::endl;
+	// delete dog1;
+	// std::cout << std::endl;
+	// dog2.displayIdea(21);
+
+	// // TESET 3 Deep copy
 	// {
 	// 	Dog dog0;
 
