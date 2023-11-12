721834d0d4e77934e45617d23756c367b7d4a106
Carl Boudard <tcarl771@gmail.com>
2023-11-13 00:15:42 +0100

    operation priority changed

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -14,8 +14,8 @@ RPN& RPN::operator=(const RPN& rhs) {
 
 bool RPN::commandLineError(std::string line) {
 
-	if (line.length() < 6) {
-		std::cerr << "Error" << std::endl;
+	if (line.length() < 5 || !isdigit(line[0])) {
+		std::cerr << "Error." << std::endl;
 		return true;
 	}
 
@@ -23,14 +23,14 @@ bool RPN::commandLineError(std::string line) {
 
 		if (!isdigit(line[i]) && line[i] != '+' && line[i] != '-' && line[i] != '*' && line[i] != '/') {
 
-			std::cerr << "Error" << std::endl;
+			std::cerr << "Error." << std::endl;
 			return true;
 		}
 
 		if (i + 1 >= line.length() || line[i + 1] == ' ')
 			i++;
 		else {		
-			std::cerr << "Error" << std::endl;
+			std::cerr << "Error." << std::endl;
 			return true;
 		}
 	}
@@ -40,75 +40,47 @@ bool RPN::commandLineError(std::string line) {
 
 void RPN::displayOperationResult(std::string line) {
 
-	int16_t i = line.length() - 1;
+	int32_t res, nb1, nb2;;
 
-	while (i >= 0) {
+	_valueStack.push(line[0] - 48);
+	for (uint32_t pos = 2; pos < line.length(); pos += 2) {
 
-		if (isdigit(line[i]))
-			_valueStack.push(line[i] - 48);
-		else
-			_signStack.push(line[i]);
-		i -= 2;
-	}
+		if (!isdigit(line[pos])) {
 
-	if (_valueStack.size() != _signStack.size() + 1) {
-		std::cerr << "Error" << std::endl;
-		return ;
-	}
+			if (_valueStack.size() >= 2) {
 
-	while (1) {
+				nb1 = _valueStack.top();
+				_valueStack.pop();
+				nb2 = _valueStack.top();
+				_valueStack.pop();
 
-		int32_t res, nb1, nb2;
 
-		nb1 = _valueStack.top();
-		_valueStack.pop();
-		nb2 = _valueStack.top();
-		_valueStack.pop();
+				if (line[pos] == '+')
+					res = nb2 + nb1;
+				else if (line[pos] == '-')
+					res = nb2 - nb1;
+				else if (line[pos] == '/') {
+					if (!nb1 || !nb2) {
+						std::cerr << "Error: division by zero." << std::endl;
+						return ;
+					}
+					res = nb2 / nb1;
+				} else
+					res = nb2 * nb1;
 
-		if (_signStack.top() == '+')
-			res = nb1 + nb2;
-		else if (_signStack.top() == '-')
-			res = nb1 - nb2;
-		else if (_signStack.top() == '/') {
-			if (!nb1 || !nb2) {
-				std::cerr << "Error: division by zero." << std::endl;
+				_valueStack.push(res);
+			}
+			else {
+
+				std::cerr << "Error 1." << std::endl;
 				return ;
 			}
-			res = nb1 / nb2;
 		} else
-			res = nb1 * nb2;
-
-		_signStack.pop();
-
-		if (_valueStack.empty() && _signStack.empty()) {
-			std::cout << res << std::endl;
-			return ;
-		}
-
-		_valueStack.push(res);
+			_valueStack.push(line[pos] - 48);
 	}
+
+	if (_valueStack.size() != 1)
+		std::cerr << "Error." << std::endl;
+	else
+		std::cout << res << std::endl;
 }
-
-/*
-	if pos == 0
-		while (pos != 2; pos += 2)
-
-				push_back if is digit == true
-				if stack. size != 2
-					error
-					return ;
-				pos += 2
-
-	if char is sign
-		if size == 2
-			calcul
-		else
-			error
-
-	if digit
-		if size == 1
-			push_back
-		else
-			error
-
-*/
