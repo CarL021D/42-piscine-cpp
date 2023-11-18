d5484a1a53e30ad469ac0ea9b81b0f21c0605354
Carl Boudard <tcarl771@gmail.com>
2023-11-19 00:08:31 +0100

    test added

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -9,25 +9,32 @@ int main() {
 	mstack.push(2);
 	mstack.push(21);
 	std::cout << "size [" << mstack.size() << "]"<< std::endl;
-	std::cout << mstack.top() << std::endl;
+	std::cout << "top: " << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.top() << std::endl;
+	std::cout << "top: " << mstack.top() << std::endl;
 	mstack.push(1);
 	mstack.push(11);
 	mstack.push(224);
 	mstack.push(31);
 
+	std::cout << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
+	std::cout << "it: " << *it << std::endl;
 	it++;
-	std::cout << *it << std::endl;
+	std::cout << "it: " << *it << std::endl;
+		it++;
+	std::cout << "it: " << *it << std::endl;
 	it--;
-	std::cout << *it << std::endl << std::endl;
+	std::cout << "it: " << *it << std::endl;
 
+	std::cout << "List: " << std::endl;
 	while (it != ite) {
-		std::cout << *it << std::endl;
+		std::cout << " " << *it << " " ;
 		++it;
 	}
+
+	std::cout << std::endl;
 	std::cout << "size [" << mstack.size() << "]"<< std::endl;
 	return 0;
 }
