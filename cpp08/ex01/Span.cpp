08402d883fc96c6d55055557ad18fcf42bfb9cbe
Carl BOUDARD <caboudar@made-f0Dr8s11.clusters.42paris.fr>
2023-11-20 18:31:23 +0100

    save

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -50,8 +50,32 @@ void	Span::addNumber(int32_t nbr) {
 
 int	Span::longestSpan() const {
 
+
+	// std::vector<int32_t> tmp;
+
+	// if (_sizeMax < 2)
+	// 	throw IndexErrorException();
+
+	// tmp = _vec;
+	// std::sort(tmp.begin(), tmp.end());
+	// int32_t maxRange = std::numeric_limits<int>::min();
+
+	// std::vector<int32_t>::const_iterator prev = tmp.begin();
+	// std::vector<int32_t>::const_iterator it = tmp.begin();
+	// ++it; // Move to the next element after the first
+
+	// for (; it != tmp.end(); ++it) {
+	// 	if (*it - *prev > maxRange) {
+	// 		maxRange = *it - *prev;
+	// 	}
+	// 	++prev;
+	// }
+
 	if (_sizeMax < 2)
 		throw IndexErrorException();
+
+		
+
 	int maxRange = std::numeric_limits<int>::min();
 	for (uint32_t i = 0; i < _vec.size() - 1; i++) {
 		for (uint32_t j = i + 1; j < _vec.size(); j++) {
